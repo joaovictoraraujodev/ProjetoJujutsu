d3ba915499f9391da090b4555aa3b92b147ac225
joaovictoraraujodev <joaovitorsms@hotmail.com>
2023-10-09 21:15:37 -0300

    cards personagens

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+// cards extensivos
+const paineis = document.querySelectorAll(".painel");
+
+paineis.forEach((painel) => {
+  painel.addEventListener("click", () => {
+    removeActiveClasses();
+    painel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  paineis.forEach((painel) => {
+    painel.classList.remove("active");
+  });
+}
